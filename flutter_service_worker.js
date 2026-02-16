d0fb3c4b19b975327ca96864d7b45424fb7acbe3
rabid jerkbrain <xxx@gmail.com>
2026-02-16 08:12:28 +0100

    Auto-Release v1.0.12+13

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "94049b574a9fc0cbcf5a9331f90f77a6
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "17ccbb30a61bc23cfa3b736f3968f129",
+"flutter_bootstrap.js": "65f03a23bf4eb4ef4edbd75e1dd38741",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
